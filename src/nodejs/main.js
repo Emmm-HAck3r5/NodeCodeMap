2064291cee1e54aa113a86c9d77c3d1d6131f4e5
bluebread <hotbread70127@gmail.com>
2017-12-18 22:34:44 +0800

    python start building

--- src/nodejs/main.js
+++ src/nodejs/main.js
@@ -1,10 +1,39 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const exec = require('child_process').execSync;
 
+const pythonFile = '../python/global.py';
+try {
+  const pythonBuffer = exec(`python ${pythonFile}`);
+  console.log(pythonBuffer.toString());
+}
+catch(err){
+  console.log(err);
+}
 // 將這個 window 物件記在全域變數裡。
 // 如果你不這麼做，這個視窗在 JavaScript 物件被 GC 後就會被自動關閉。
 let win;
+
+// 這個方法在 Electron 初始化完成，準備好建立瀏覽器視窗時會被叫用。
+// 有些 API 只能在這個事件發生後才能用。
+app.on('ready', createWindow);
+
+// 在所有視窗都關閉時結束程式。
+app.on('window-all-closed', () => {
+  // 在 macOS 裡，普遍的作法是將應用程式及選單列繼續留著，直到使用者按了 Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // 在 macOS 裡，一般會在使用者按了 Dock 圖示且沒有其他視窗開啟的情況下，
+  // 重新在應用程式裡建立視窗。
+  if (win === null) {
+    createWindow();
+  }
+});
 
 function createWindow () {
   // 建立瀏覽器視窗。
@@ -29,23 +58,3 @@ function createWindow () {
     win = null;
   });
 }
-
-// 這個方法在 Electron 初始化完成，準備好建立瀏覽器視窗時會被叫用。
-// 有些 API 只能在這個事件發生後才能用。
-app.on('ready', createWindow);
-
-// 在所有視窗都關閉時結束程式。
-app.on('window-all-closed', () => {
-  // 在 macOS 裡，普遍的作法是將應用程式及選單列繼續留著，直到使用者按了 Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // 在 macOS 裡，一般會在使用者按了 Dock 圖示且沒有其他視窗開啟的情況下，
-  // 重新在應用程式裡建立視窗。
-  if (win === null) {
-    createWindow();
-  }
-});

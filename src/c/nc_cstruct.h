dfbba248411efdf9a7a1af21345e0e9d527db5e7
bluebread <hotbread70127@gmail.com>
2017-12-22 18:47:44 +0800

    update nc_cstruct

--- src/c/nc_cstruct.h
+++ src/c/nc_cstruct.h
@@ -20,30 +20,25 @@
 #include "nc_io.h"
 //#include "nc_clex.h"
 
-typedef enum NC_CFileType // 16进制
+typedef enum NC_CFileType
 {
-	NC_CFile_H = 0x1, // .h
-	NC_CFile_C = 0x2, // .c
-	NC_CFile_STD = 0x10 // std.h
+	NC_CFile_H = 0x1, NC_CFile_C = 0x2,
+	NC_CFile_STD = 0x10
 }NC_CFileType;
-
-
 typedef struct NC_CFile
 {
 	struct NC_CFile *parent;
 	struct NC_CFile *lchild, *rchild;
 	NC_CFileType file_type;
-	struct NC_CCompInfo *comp_info; 
-	struct NC_CTokenStream *token_stream;//ignore
+	struct NC_CCompInfo *comp_info;
+	struct NC_CTokenStream *token_stream;
 	struct NC_CFunction *function_list;
 	struct NC_CVariable *var_list;
 	struct NC_CMacro *macro_list;
 	struct NC_CType *type_list;
-	EH_Array *include_arr; //ignore
+	EH_Array *include_arr;
 }NC_CFile;
 
-
-// ignore
 typedef struct NC_CTokenStream
 {
 	CToken *stream;
@@ -52,25 +47,24 @@ typedef struct NC_CTokenStream
 
 typedef struct NC_CCompInfo
 {
-	EH_String *file_data; // the content of the file
-	char *file_path; // the path ..
-	char *file_name; // ignore
-	EH_String *decl; // the full code  content
-	u32 lineno; //  行数
-	u32 pos; //ֵ ignore
+	EH_String *file_data;
+	char *file_path;
+	EH_String *decl;
+	u32 lineno;
+	u32 pos;//�ñ������ڻ�ȡdecl���洢����ftell����ֵ
 }NC_CFileInfo;
-typedef enum NC_CTypeType // 16
+typedef enum NC_CTypeType
 {
 	NC_CType_Basic = 0x1,
 	NC_CType_Struct = 0x2,
 	NC_CType_Enum = 0x4,
 	NC_CType_Union = 0x8,
-	NC_CType_HBasic = 0x10,// ignore
+	NC_CType_HBasic = 0x10,//�����unsigned����signed �����unsigned���λ��1
 	NC_CType_Pointer = 0x20,
 	NC_CType_Array = 0x40,
 }NC_CTypeType;
 
-typedef enum NC_CTypeModifier // 16
+typedef enum NC_CTypeModifier
 {
 	NC_CTypeModifier_Const = 0x1,
 	NC_CTypeModifier_Static = 0x2,
@@ -79,17 +73,17 @@ typedef enum NC_CTypeModifier // 16
 	NC_CTypeModifier_Volatile = 0x10,
 	NC_CTypeModifier_Auto = 0x20
 }NC_CTypeModifier;
-typedef struct NC_CType // chain structure
+typedef struct NC_CType
 {
 	struct NC_CType *next, *prev;
-	u8 type_type; 
+	u8 type_type;
 	EH_String *type_name;
 	NC_CCompInfo *comp_info;
 	u8 type_modifier;//�˴�constָ�����Ƿ���const
-	struct NC_CVariable *member; // 存畜类的成员变量
+	struct NC_CVariable *member;
 }NC_CType;
 
-typedef struct NC_CVariable //变量
+typedef struct NC_CVariable
 {
 	struct NC_CVariable *next, *prev;
 	NC_CType var_type;
@@ -114,12 +108,11 @@ typedef struct NC_CFunction
 	EH_String *func_name;
 	NC_CCompInfo *comp_info;
 	u8 func_type;
-	NC_CType func_ret_type; // the type function return
-	struct NC_CVariable *parameter; // params of function
-	EH_String *func_body; // the code of function
+	NC_CType func_ret_type;
+	struct NC_CVariable *parameter;
+	EH_String *func_body;
 }NC_CFunction;
 
-// ignore
 enum Mcotype { rplace, function };
 
 typedef struct NC_CMacro
@@ -128,7 +121,7 @@ typedef struct NC_CMacro
 	EH_String *macro_name;
 	NC_CCompInfo *comp_info;
 	EH_String *parameter;
-	EH_String *after_expand; //展开后的红
+	EH_String *after_expand;
 
 	Mcotype macrotype = rplace;
 	CToken *macro_name_tokens;
@@ -136,22 +129,14 @@ typedef struct NC_CMacro
 	//add function pointer
 }NC_CMacro;
 
-
-// ignore
 typedef struct NC_Include
 {
 	char* name;
 	char* path;
 	int type;
-	EH_Array *ptrs;
 }NC_Include;
-
-#define NC_STD 1
-#define NC_CST 2
 
 NC_CFile* nc_cfile_init(NC_File *fp);
 NC_CCompInfo* nc_ccompinfo_init(void);
-
-NC_CFile *nc_cfile_search(char *name);
 NC_Include* nc_include_init();
 #endif

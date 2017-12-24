de5b8fe479bec3d4316f7fc628469d54b6d7de20
Xie <jujianai@hotmail.com>
2017-12-24 08:19:53 +0800

    debug c

--- src/c/nc_dock.cpp
+++ src/c/nc_dock.cpp
@@ -13,3 +13,21 @@ NC_CFile *nc_dock_generate(const char *dir)
 	}
 	return file_list;
 }
+
+//NC_CFile *nc_dock_generate()
+//{
+//	//FILE *txtf=fopen("getpath.txt","r");
+//	char *dir=(char*)malloc(1000);
+//	strcpy(dir,"C:\\Users\\hotbr\\Desktop\\Projects\\NodeCodeMap\\src\\c\\");
+//	//fscanf(txtf,"%s", dir);
+//	nc_file_dep_generate(dir);
+//	int i;
+//	NC_CFile *f = (NC_CFile*)malloc(sizeof(NC_CFile));
+//	for (i = 0; i < cfile_array->elmcount; i++)
+//	{
+//		f = (NC_CFile*)cfile_array->data[i];
+//		nc_parse_translation_unit(f);
+//		nc_parse_get_decl(f);
+//	}
+//	return file_list;
+//}
--- src/c/nc_dock.h
+++ src/c/nc_dock.h
@@ -4,7 +4,9 @@
 #include "nc_cstruct.h"
 #include "nc_filedep.h"
 #include "nc_parser.h"
+#include <stdio.h>
 
 extern "C" __declspec(dllexport) NC_CFile *nc_dock_generate(const char *dir);
+//extern "C" __declspec(dllexport) NC_CFile *nc_dock_generate();
 
 #endif
